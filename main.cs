d5843542a68a0180e2ee10dbddbcb9a52d78de1d
Александр Чубанов <99904396+AlexBridge69@users.noreply.github.com>
2022-05-09 15:57:03 +0000

    Описан метод, который в исходном массиве считает количество элементов, соответствующих условию.

--- main.cs
+++ main.cs
@@ -1,7 +1,22 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    static int NumberOfElementsForNewArray(string[] arrayInput)
+    {
+        int NumberOfElements = 0;
+        for (int i = 0; i < arrayInput.Length; i++)
+        {
+            if (arrayInput[i].Length <= 3)
+            {
+                NumberOfElements += 1;
+            }
+        }
+        return NumberOfElements;
+    }
+
+    public static void Main()
+    {
+
+    }
 }
